c2837ae1a4976c342512ee0d694f1459d1a804c1
George <prikashchenkov@tehnokod.com>
2022-12-22 22:41:20 +0300

    Реализован OAP

---
+++ src/main/java/ru/yandex/practicum/filmorate/aop/feed/EventAspect.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.aop.feed;
+
+/**
+ * @project java-filmorate
+ * @auther George Prikashchenkov on 22.12.2022
+ */
+public class EventAspect {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/aop/feed/RemoveEvent.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.aop.feed;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @project java-filmorate
+ * @auther George Prikashchenkov on 22.12.2022
+ */
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface RemoveEvent {
+}
